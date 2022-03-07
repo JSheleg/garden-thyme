1b3e127bcc5e191aba020a500cd775ad609ac7c3
katredford <katharine.redford@gmail.com>
2022-03-07 11:17:58 -0600

    add airplant cloud config

---
+++ backEnd/airPlant-cloud-config/src/test/java/com/company/airPlantcloudconfig/AirPlantCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.airPlantcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirPlantCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
